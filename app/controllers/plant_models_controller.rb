6d977543d681893e5eb226c87056af72d4626803
nkiru onwuneme <generalhrh@gmail.com>
2018-01-22 16:50:55 -0800

    added plant controller actions

--- app/controllers/plant_models_controller.rb
+++ app/controllers/plant_models_controller.rb
@@ -1,3 +1,21 @@
 class PlantModelsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   resources :sensors
   resources :readings
+  resources :plant_models
 
 
 end
