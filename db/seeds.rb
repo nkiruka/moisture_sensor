745be5da3f63bd40e41f6df6b0a8382dd016f47b
nkiru onwuneme <generalhrh@gmail.com>
2018-01-23 12:44:05 -0800

    modified sensor id

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,24 +10,27 @@ Sensor.create!([
   {name: "Sensor 1", description: "moisture ", model: "Y-161"},
   {name: "Sensor 2", description: "moisture", model: "Y-161"}
 ])
+sensor1 = Sensor.first
+sensor2 = Sensor.last
+
 Reading.create!([
-  {sensor_id: 1, value: 50, created_at: Time.now},
-  {sensor_id: 1,value: 55, created_at: Time.now+2},
-  {sensor_id: 1,value: 60, created_at: Time.now+3},
-  {sensor_id: 1,value: 65, created_at: Time.now+4},
-  {sensor_id: 1,value: 70, created_at: Time.now+5},
-  {sensor_id: 1,value: 75, created_at: Time.now+6},
-  {sensor_id: 1,value: 80, created_at: Time.now+7},
-  {sensor_id: 1,value: 85, created_at: Time.now+8},
-  {sensor_id: 1,value: 90, created_at: Time.now+9},
-  {sensor_id: 1,value: 95, created_at: Time.now+10},
-  {sensor_id: 2, value: 30, created_at: Time.now},
-  {sensor_id: 2, value: 29, created_at: Time.now+2},
-  {sensor_id: 2, value: 29, created_at: Time.now+3},
-  {sensor_id: 2, value: 28, created_at: Time.now+4},
-  {sensor_id: 2, value: 20, created_at: Time.now+5},
-  {sensor_id: 2, value: 30, created_at: Time.now+6},
-  {sensor_id: 2, value: 31, created_at: Time.now+7},
-  {sensor_id: 2, value: 31, created_at: Time.now+8},
-  {sensor_id: 2, value: 32, created_at: Time.now+9}
+  {sensor: sensor1, value: 50, created_at: Time.now},
+  {sensor: sensor1,value: 55, created_at: Time.now+2},
+  {sensor: sensor1,value: 60, created_at: Time.now+3},
+  {sensor: sensor1,value: 65, created_at: Time.now+4},
+  {sensor: sensor1,value: 70, created_at: Time.now+5},
+  {sensor: sensor1,value: 75, created_at: Time.now+6},
+  {sensor: sensor1,value: 80, created_at: Time.now+7},
+  {sensor: sensor1,value: 85, created_at: Time.now+8},
+  {sensor: sensor1,value: 90, created_at: Time.now+9},
+  {sensor: sensor1,value: 95, created_at: Time.now+10},
+  {sensor: sensor2, value: 30, created_at: Time.now},
+  {sensor: sensor2, value: 29, created_at: Time.now+2},
+  {sensor: sensor2, value: 29, created_at: Time.now+3},
+  {sensor: sensor2, value: 28, created_at: Time.now+4},
+  {sensor: sensor2, value: 20, created_at: Time.now+5},
+  {sensor: sensor2, value: 30, created_at: Time.now+6},
+  {sensor: sensor2, value: 31, created_at: Time.now+7},
+  {sensor: sensor2, value: 31, created_at: Time.now+8},
+  {sensor: sensor2, value: 32, created_at: Time.now+9}
 ])
