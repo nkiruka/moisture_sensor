f3efbb11764fbaca652d0eafbb0b5a67ea4fdd2f
nkiru onwuneme <generalhrh@gmail.com>
2018-01-23 17:42:31 -0800

    add model association relationships

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,5 +1,6 @@
 class Reading < ApplicationRecord
   belongs_to :sensor
+  belongs_to :plant
 
   scope :timestamps, ->(select_statment) do
     select(select_statment).group("sensor_id, interval_timestamp")
